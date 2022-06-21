27e18daab55fb373de2b179949441efb70e1536e
Zonglei Dong <dongzonglei@apache.org>
2022-06-21 10:34:43 +0800

    add map test code.

---
+++ map/map_test.go
@@ -0,0 +1,24 @@
+package _map
+
+import "testing"
+
+const mapSize = 10000
+
+// go test -bench=.
+func BenchmarkMapInitWithoutCap(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		m := make(map[int]int)
+		for i := 0; i < mapSize; i++ {
+			m[i] = i
+		}
+	}
+}
+
+func BenchmarkMapInitWithCap(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		m := make(map[int]int, mapSize)
+		for i := 0; i < mapSize; i++ {
+			m[i] = i
+		}
+	}
+}
