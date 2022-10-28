0752b9e5c7eabd1e74aaa9338ee68baad1babd9c
Zonglei Dong <dongzonglei@apache.org>
2022-10-28 16:19:44 +0800

    add bench test demo code.

--- bench_test/fib_test.go
+++ bench_test/fib_test.go
@@ -1,10 +1,16 @@
 package bench_test
 
-import "testing"
+import (
+	bench "github.com/arana-db/arana/bench_test"
+	"testing"
+	"time"
+)
 
 // https://geektutu.com/post/hpg-benchmark.html
 func BenchmarkFib(b *testing.B) {
+	time.Sleep(time.Second * 3) // 模拟耗时准备任务
+	b.ResetTimer()              // 重置定时器
 	for n := 0; n < b.N; n++ {
-		fib(30)
+		bench.Fib(30)
 	}
 }
---
+++ bench_test/memory_bench_test.go
@@ -0,0 +1,67 @@
+package bench_test
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func generateWithCap(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	nums := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Int())
+	}
+	return nums
+}
+
+func generate(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	nums := make([]int, 0)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Int())
+	}
+	return nums
+}
+
+func BenchmarkGenerateWithCap(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		generateWithCap(1000000)
+	}
+}
+
+func BenchmarkGenerate(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		generate(1000000)
+	}
+}
+
+func benchmarkGenerate(i int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		generate(i)
+	}
+}
+
+func bubbleSort(nums []int) {
+	for i := 0; i < len(nums); i++ {
+		for j := 1; j < len(nums)-i; j++ {
+			if nums[j] < nums[j-1] {
+				nums[j], nums[j-1] = nums[j-1], nums[j]
+			}
+		}
+	}
+}
+
+func BenchmarkGenerate1000(b *testing.B)    { benchmarkGenerate(1000, b) }
+func BenchmarkGenerate10000(b *testing.B)   { benchmarkGenerate(10000, b) }
+func BenchmarkGenerate100000(b *testing.B)  { benchmarkGenerate(100000, b) }
+func BenchmarkGenerate1000000(b *testing.B) { benchmarkGenerate(1000000, b) }
+
+func BenchmarkBubbleSort(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+		nums := generateWithCap(10000)
+		b.StartTimer()
+		bubbleSort(nums)
+	}
+}
