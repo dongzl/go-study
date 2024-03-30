4f4731ca42993a406a980c78d95f28d607444fba
dongzonglei <dongzonglei@jd.com>
2024-03-30 17:50:04 +0800

    zookeeper

--- go.mod
+++ go.mod
@@ -7,17 +7,17 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/golang/mock v1.6.0
 	github.com/henrygas/go-pprof-example/data v0.0.0-20200802162326-277a65794482
+	github.com/samuel/go-zookeeper v0.0.0-20201211165307-7117e9ea2414
 	github.com/stretchr/testify v1.7.0
 	github.com/tidwall/gjson v1.14.1
 	golang.org/x/exp v0.0.0-20220613132600-b0d781184e0d
+	google.golang.org/grpc v1.51.0
+	google.golang.org/protobuf v1.27.1
 )
 
 require (
-	github.com/chzyer/readline v1.5.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/pprof v0.0.0-20220729232143-a41b82acbcb1 // indirect
-	github.com/ianlancetaylor/demangle v0.0.0-20220517205856-0058ec4f073c // indirect
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
@@ -27,9 +27,6 @@ require (
 	golang.org/x/sys v0.0.0-20220731174439-a90be440212d // indirect
 	golang.org/x/text v0.4.0 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
-	google.golang.org/grpc v1.51.0 // indirect
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ zoo/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/samuel/go-zookeeper/zk"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	// 创建zk连接地址
+	hosts := []string{"127.0.0.1:2181"}
+	// 连接zk
+	conn, _, err := zk.Connect(hosts, time.Second*5)
+	defer conn.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	println(conn.Server())
+
+	export(conn, "/governance_ds/metadata/sys/schemas/sys/tables", "sys")
+	export(conn, "/governance_ds/metadata/performance_schema/schemas/performance_schema/tables", "performance_schema")
+	export(conn, "/governance_ds/metadata/mysql/schemas/mysql/tables", "mysql")
+	export(conn, "/governance_ds/metadata/information_schema/schemas/information_schema/tables", "information_schema")
+}
+
+func export(conn *zk.Conn, path string, dir string) {
+	data, _, err := conn.Children(path)
+	if err != nil {
+		fmt.Printf("查询%s失败, err: %v\n", path, err)
+		return
+	}
+	fmt.Printf("%s 的值为 %s\n", path, len(data))
+	for i := 0; i < len(data); i++ {
+		write(conn, path, data[i], dir)
+	}
+}
+
+func write(conn *zk.Conn, path string, child string, dir string) {
+	fmt.Printf("%s 的子节点为 %s\n", path, child)
+	leaf, _, _ := conn.Get(path + "/" + child + "/versions/0")
+	//fmt.Printf("%s 的值为 %s\n", path, string(leaf))
+	f, err := os.OpenFile("zoo/"+dir+"/"+child+".yaml", os.O_RDONLY|os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	defer f.Close()
+	if err != nil {
+		log.Println("open file error :", err)
+		return
+	}
+	_, err = f.WriteString(string(leaf))
+	if err != nil {
+		log.Println("write file error :", err)
+		return
+	}
+}
